820ecd0bf8dc065ae8acd94459e41d706c24b7e4
Quang-Linh LE <linktohack@gmail.com>
2016-03-08 15:55:55 +0100

    Add org-breadcrumbs

---
+++ org-breadcrumbs.el
@@ -0,0 +1,61 @@
+;;; org-breadcrumbs.el --- Breadcrumbs for Org.
+
+;; Copyright (C) 2016 Quang-Linh LE
+
+;; Author: Quang-Linh LE <linktohack@gmail.com>
+;; URL: http://github.com/linktohack/org-breadcrumbs
+;; Version: 0.0.7
+;; Keywords: org-breadcrumbs breadcrumbs breadcrumb focus narrow
+;; Package-Requires: ()
+
+;; This file is not part of GNU Emacs.
+
+;;; Code:
+
+(setq org-breadcrumbs-header-format
+      '(:eval
+        (let ((path (org-get-outline-path t)))
+          (mapconcat #'identity
+                     (cons
+                      (let ((map (make-sparse-keymap)))
+                        (define-key map [header-line mouse-1]
+                          #'(lambda ()
+                              (interactive)
+                              (widen)
+                              (org-content 1)
+                              (goto-char (point-min))))
+                        (concat
+                         (propertize
+                          " "
+                          'display
+                          '(space :align-to 0))
+                         (propertize
+                          "Top"
+                          'font-lock-face 'org-link
+                          'mouse-face 'highlight
+                          'keymap map)))
+                      (cl-loop
+                       for i downfrom (- (length path) 1)
+                       for heading in path
+                       collect (let ((map (make-sparse-keymap)))
+                                 (define-key map [header-line mouse-1] 
+                                   `(lambda ()
+                                      (interactive)
+                                      (widen)
+                                      (outline-up-heading ,i)
+                                      (org-narrow-to-subtree)))
+                                 (propertize
+                                  heading
+                                  'font-lock-face 'org-link
+                                  'mouse-face 'highlight
+                                  'keymap map))))
+                     " > "))))
+
+(define-minor-mode org-breadcrumbs-mode
+  "Breadcrumbs for Org."
+  :lighter " b"
+  (if org-breadcrumbs-mode
+      (setq-local header-line-format org-breadcrumbs-header-format)
+    (setq-local header-line-format nil)))
+
+(provide 'org-breadcrumbs)
